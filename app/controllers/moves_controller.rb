94a74659928d3faac37f40f3606d6fd623f90518
alejandro toledo <atoledofr@gmail.com>
2021-10-26 17:55:16 -0500

    add validations for move model and methods for move controller

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,2 +1,13 @@
 class MovesController < ApplicationController
+
+  def create
+    @move = Move.new(move_params)
+  end
+
+  private
+  def move_params
+    params.require(:move).permit(:game, :col, :row, :number)
+  end
+  
+  
 end
--- app/models/move.rb
+++ app/models/move.rb
@@ -1,8 +1,12 @@
 class Move < ApplicationRecord
   belongs_to :game
   
-  validates :board, presence: true
-  validates :row, presence: true
-  validates :column, presence: true
-  validates :number, presence: true
+  validates :board, presence: true, numericality: { only_integer: true }
+
+  validates :row, presence: true, numericality: { only_integer: true }
+
+  validates :column, presence: true, numericality: { only_integer: true }
+
+  validates :number, presence: true, numericality: { only_integer: true }
+  
 end
