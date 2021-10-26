e0e00e500a4322f76c934e793d9994d9f5dbdbb0
alejandro toledo <atoledofr@gmail.com>
2021-10-26 17:50:23 -0500

    add solution for the game and method to create new games

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,2 +1,14 @@
 class GameController < ApplicationController
+
+  def create
+    @game = Game.create!(game_params)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:board, :solution)
+  end
+  
+  
 end
--- db/migrate/20211026201810_create_games.rb
+++ db/migrate/20211026201810_create_games.rb
@@ -2,6 +2,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
       t.string :board
+      t.string :solution
 
       t.timestamps
     end
