f7159649cb3ad1e6a0bb32e2e58c4c7bdac85de7
alejandro toledo <atoledofr@gmail.com>
2021-10-26 18:46:54 -0500

    add parse_board, find_empty_positions, check col and check row for moves controller

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -4,6 +4,53 @@ class MovesController < ApplicationController
     @move = Move.new(move_params)
   end
 
+  def check_row(board, row, number)
+    for col in 0...board[row].length
+      if board[row][col] == number
+        return false
+      end
+    end
+  
+    true
+  end
+
+  def check_col(board, col, number)
+    for row in 0...board.length
+      if board[row][col] == number
+        return false
+      end
+    end
+  
+    true
+  end
+
+  def parse_board(board_string)
+    # @board_string = @game.board
+    @result = []
+
+    for i in 0...board_string.length do
+      if i % 9 == 0
+        @result.push(board_string[i...i + 9].split("").map(&:to_i))
+      end
+    end
+    @result
+    # render json: {board: @result}, status: :ok
+  end
+
+  def find_empty_positions(board)
+    empty_positions = []
+  
+    for row in 0...board.length
+      for col in 0...board[row].length
+        if board[row][col] == 0
+          empty_positions << [row, col]
+        end
+      end
+    end
+  
+    empty_positions
+  end
+
   private
   def move_params
     params.require(:move).permit(:game, :row, :col, :number)
