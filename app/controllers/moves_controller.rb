b18b13f5ef2a457a1ee511cc82d3fbc6dd15cf92
alejandro toledo <atoledofr@gmail.com>
2021-10-27 11:00:35 -0500

    add validation for rows and col in move controller

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -131,42 +131,6 @@ class MovesController < ApplicationController
     end
     
   end
-  
-
-  # def solve(board_string)
-  #   board = parse_board(board_string)
-  #   empty_positions = find_empty_positions(board)
-  
-  #   solve_puzzle(board, empty_positions)
-  # end
-
-  # def solve_puzzle(board, empty_positions)
-  #   i = 0
-    
-  #   while i < empty_positions.length
-  #     row = empty_positions[i][0]
-  #     column = empty_positions[i][1]
-  #     number = board[row][column] + 1
-  #     found = false
-  
-  #     while !found && number <= 9
-  #       if check_value(board, row, column, number)
-  #         found = true
-  #         board[row][column] = number
-  #         i += 1
-  #       else
-  #         number += 1
-  #       end
-  #     end
-  
-  #     if !found
-  #       board[row][column] = 0
-  #       i -= 1
-  #     end
-  #   end
-    
-  #   board
-  # end
 
   def update_board(board, id, row, col, number, solution)
     @game = Game.find(id)
--- app/models/move.rb
+++ app/models/move.rb
@@ -3,12 +3,10 @@ class Move < ApplicationRecord
   
   validates :game_id, presence: true, numericality: { only_integer: true } 
 
-  validates :row, presence: true, numericality: { only_integer: true }
+  validates :row, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, ess_than_or_equal_to: 8 }
 
-  validates :col, presence: true, numericality: { only_integer: true }
+  validates :col, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, ess_than_or_equal_to: 8 }
 
   validates :number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, ess_than_or_equal_to: 9 }
-
-  # :greater_than_or_equal_to, :less_than_or_equal_to
 
 end
