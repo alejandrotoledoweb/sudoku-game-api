662e21f36bcae9bfcd22852fd14b3ca882fe36bd
alejandro toledo <atoledofr@gmail.com>
2021-10-26 19:43:24 -0500

    add parse_solution and solve_puzzle method for move

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -2,6 +2,13 @@ class MovesController < ApplicationController
 
   def create
     @move = Move.new(move_params)
+    @game = Game.find(params[:@move.game])
+    board = parse_board(@game.board)
+    row = @move.row
+    col = @move.col
+    number = @move.number
+    check_value(board, row, col, number)
+    
   end
 
   def check_value(board, row, col, number)
@@ -60,6 +67,19 @@ class MovesController < ApplicationController
     # render json: {board: @result}, status: :ok
   end
 
+  def parse_solution(board_string)
+    # @board_string = @game.board
+    result = []
+
+    for i in 0...board_string.length do
+      if i % 9 == 0
+        result.push(board_string[i...i + 9].split("").map(&:to_i))
+      end
+    end
+    result
+    # render json: {board: @result}, status: :ok
+  end
+
   def find_empty_positions(board)
     empty_positions = []
   
@@ -79,6 +99,34 @@ class MovesController < ApplicationController
     empty_positions = find_empty_positions(board)
   
     solve_puzzle(board, empty_positions)
+  end
+
+  def solve_puzzle(board, empty_positions)
+    i = 0
+    
+    while i < empty_positions.length
+      row = empty_positions[i][0]
+      column = empty_positions[i][1]
+      number = board[row][column] + 1
+      found = false
+  
+      while !found && number <= 9
+        if check_value(board, row, column, number)
+          found = true
+          board[row][column] = number
+          i += 1
+        else
+          number += 1
+        end
+      end
+  
+      if !found
+        board[row][column] = 0
+        i -= 1
+      end
+    end
+    
+    board
   end
 
   private
